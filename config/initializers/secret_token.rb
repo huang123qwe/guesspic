14084cf51d6cf33e1a3444fe4546e62a92d4e934
pulsen <huang123qwe@126.com>
2014-02-28 10:48:17 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Guesspic::Application.config.secret_key_base = 'f895cd7ab7266e9d39a8a7b7d419423551d1611a553f8f4044895cb4719324a2d781ea3a13d668d995400f9c0d2f7ef3a55b48e0df7020e2e4398de0cc3b78f2'
